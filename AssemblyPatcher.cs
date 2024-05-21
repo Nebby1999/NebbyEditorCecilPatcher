c0885d073c213b0e0159001772cb9bafffadb802
Nebby <nebby131999@gmail.com>
2024-05-20 20:42:00 -0400

    First Push

---
+++ AssemblyPatcher.cs
@@ -0,0 +1,101 @@
+﻿using Mono.Cecil;
+
+namespace EditorPatcher
+{
+    public class AssemblyPatcher
+    {
+        private PatchData[] _patchData;
+        private AssemblyDefinition _assembly;
+        public void Patch()
+        {
+            foreach(PatchData patchData in _patchData)
+            {
+                patchData.DoPatch();
+            }
+        }
+
+        public AssemblyPatcher(string[] patches, ReaderParameters readerParams, AssemblyDefinition assemblyDefinition)
+        {
+            _assembly = assemblyDefinition;
+            _patchData = new PatchData[patches.Length];
+            for(int i = 0; i < patches.Length; i++)
+            {
+                _patchData[i] = new PatchData(patches[i], assemblyDefinition);
+            }
+        }
+
+        private class PatchData
+        {
+            public TypeDefinition TargetTypeDefinition { get; set; }
+
+            public PatchType TypeOfPatch { get; set; }
+            public void DoPatch()
+            {
+                switch (TypeOfPatch)
+                {
+                    case PatchType.Invalid:
+                        break;
+                    case PatchType.Field:
+                        PatchField();
+                        break;
+                    case PatchType.Property:
+                        PatchProperty();
+                        break;
+                    case PatchType.Method:
+                        PatchMethod();
+                        break;
+                }
+            }
+
+            private void PatchField()
+            {
+
+            }
+
+            private void PatchProperty()
+            {
+
+            }
+
+            private void PatchMethod()
+            {
+
+            }
+
+            public PatchData()
+            {
+
+            }
+
+            public PatchData(string patch, AssemblyDefinition definition)
+            {
+                string[] patchAsArray = patch.Split(',');
+
+                TargetTypeDefinition = definition.MainModule.GetType(patchAsArray[0]);
+
+                PatchType patchType = PatchType.Invalid;
+                switch(patchAsArray[1])
+                {
+                    case "FIELD":
+                        patchType = PatchType.Field;
+                        break;
+                    case "PROPERTY":
+                        patchType = PatchType.Property;
+                        break;
+                    case "METHOD":
+                        patchType = PatchType.Method;
+                        break;
+                }
+                TypeOfPatch = patchType;
+            }
+
+            public enum PatchType
+            {
+                Invalid,
+                Field,
+                Property,
+                Method
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using Mono.Cecil;
+
+namespace EditorPatcher
+{
+    internal class Program
+    {
+        /*
+         * 0 = Path of assembly to patch
+         * 1 = Output path
+         * 2 = Path to directory with required game assemblies
+         * 3 = List of fields to patch.
+         */
+
+        private static void Main(string[] args)
+        {
+
+        }
+        /*static void Main(string[] args)
+        {
+            ProgramArguments arguments = new ProgramArguments(args);
+
+            var resolver = new DefaultAssemblyResolver();
+
+            resolver.AddSearchDirectory(arguments.pathToRequiredAssembliesDirectory);
+
+            var readerParams = new ReaderParameters()
+            {
+                AssemblyResolver = resolver,
+            };
+
+            if(File.Exists(arguments.pathOfAssemblyToPatch))
+            {
+                resolver.AddSearchDirectory(Path.GetDirectoryName(arguments.pathOfAssemblyToPatch));
+
+                string fileOutputPath = arguments.outputPath;
+                if(!PatchAssembly(arguments.pathOfAssemblyToPatch, fileOutputPath, arguments.patchesArray, readerParams))
+                {
+                    //Idk something i guess.
+                }
+            }
+        }
+
+        private static bool PatchAssembly(string assemblyPath, string outputPath, string[] patches, ReaderParameters readerParams)
+        {
+            try
+            {
+                using var memoryStream = new MemoryStream(File.ReadAllBytes(assemblyPath));
+                using var assemblyDefinition = AssemblyDefinition.ReadAssembly(memoryStream, readerParams);
+
+                AssemblyPatcher patcher = new AssemblyPatcher(assemblyPath, outputPath, patches, assemblyDefinition);
+                patcher.Patch();
+
+                using var tempStream = new MemoryStream();
+
+                assemblyDefinition.Write(tempStream);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+            return true;
+
+        }
+
+
+        private class ProgramArguments
+        {
+            public string pathOfAssemblyToPatch;
+            public string outputPath;
+            public string pathToRequiredAssembliesDirectory;
+            public string[] patchesArray;
+            
+            public ProgramArguments(string[] arguments)
+            {
+                pathOfAssemblyToPatch = arguments[0];
+                outputPath = arguments[1];
+                pathToRequiredAssembliesDirectory = arguments[2];
+
+                //Obtain the total length of the arguments.
+                var argumentsLength = arguments.Length;
+                //Reduce by 3, to calculate size of the patches array
+                argumentsLength -= 3;
+                patchesArray = new string[argumentsLength];
+
+                for(int i = 3; i < argumentsLength + 1; i++)
+                {
+                    patchesArray[i] = arguments[i];
+                }
+            }
+
+            public ProgramArguments(string pathOfAssemblyToPatch, string outputPath, string pathToRequiredAssembliesDirectoru, params string[] patchesArray)
+            {
+                this.pathOfAssemblyToPatch = pathOfAssemblyToPatch;
+                this.outputPath = outputPath;
+                this.pathToRequiredAssembliesDirectory = pathToRequiredAssembliesDirectoru;
+                this.patchesArray = patchesArray;
+            }
+        }*/
+    }
+}
